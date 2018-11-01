0cb7a961df9f966a1cf0ce69a8de057b726df6f9
Tambria Kemp <info@cre8visions.com>
2018-11-01 13:05:38 -0400

    refactored javascript in js.js file

--- js/index.js
+++ js/index.js
@@ -1,69 +1,58 @@
-'use strict';
+$(function initApp() {
+    'use strict';
 
 
-//function for getting youtube data
-// function getDataFromYoutube () {
+    $('#search-form').on('submit', function (ev) {
+        ev.preventDefault();
+        console.log('For submitted! But we are not doing anything - yet.');
 
-// }
+        let searchCriteria = $('#query').val();
+        console.log({ searchCriteria });
 
-//handle search button click
-function handleSearchButton (){
-    $('.form-container').on('submit','#start-button', function(event){
-        event.preventDefault();
+        $.get("https://www.googleapis.com/youtube/v3/search", {
+            part: 'snippet, id',
+            q: searchCriteria,
+            type: 'video',
+            key: 'AIzaSyDt5iuCf8m9QH2C_6hwNQF76W_mFnsfers'
+        },  function (data) {
+                console.log('We got an answer:', data);
+
+                if (data.items.length === 0) {
+                    $('.videos').append(`<p>Sorry, we can't find any results for that search criteria. Please search again.</p>`)
+                    // show the user that we've got no results from YouTube
+
+                    //  return 
+                } else {
+                    const results = data.items.map((item, index) => renderResult(item));
+                    $('.videos').append(`
+                    <div>
+                      <h2>
+                      <a class="js-result-name" href="${result.html_url}" target="_blank">${result.name}</a> by <a class="js-user-name" href="${result.owner.html_url}" target="_blank">${result.owner.login}</a></h2>
+                      <p>Number of watchers: <span class="js-watchers-count">${result.watchers_count}</span></p>
+                      <p>Number of open issues: <span class="js-issues-count">${result.open_issues}</span></p>
+                    </div>
+                  `);
+
+                }
+                //     $('.videos').append(`<div class="grid">${}</div>`);
+
+
+                //     // let html = "";
+                //     // let entries = data.items;
+                    
+                //     // $.each(entries, function (index, data) {
+                //     //     let title = data.snippet.title;
+                //     //     let thumb = data.snippet.thumbnails.high.url;
+
+                //         // html += '<p>' + title + '</p>';
+                //         // html += '<img src="' + thumb + '">';
+
+                //     // }); 
+                    
+                //     // $('.videos').append(`<div class="grid">${html}</div>`);
+                // }
+            
+                
+        });                
     });
-
-    handleSearch();
-}
-
-//function for search
-function handleSearch () {
-    $('.form-container').on('click','#start-button', function(event){
-        event.preventDefault();
-    });
-    //clear results
-    $('.videos').html('');
-    $('#search-button').html('');
-
-    //get form input
-   let q = $('#query').val();
-
-    //Run GET request on api
-    $.get("https://www.googleapis.com/youtube/v3/search",{
-        part: 'snippet, id',
-        q: q,
-        type: 'video',
-        key: 'AIzaSyDt5iuCf8m9QH2C_6hwNQF76W_mFnsfers'},
-        function(data) {
-            let nextPageToken = data.nextPageToken;
-            let prevPageToken = data.prevPageToken;
-
-            console.log(data);
-
-    });
-
-    // $('search-button').on('click', function (event) {
-
-    //     $('videos').append('')
-
-    // })
-}
-
-// function displaySearch(event) {
-// //clear results
-
-
-//     $('videos').append(`
-//             <div class="3col-container">
-
-//             </div>`)
-// }
-
-
-//event listener
-function watchSubmit() {
-    handleSearchButton();
-    handleSearch();
-}
-
-
-$(watchSubmit);
+});
---
+++ js/js.js
@@ -0,0 +1,54 @@
+'use strict';
+
+function initApp() {
+    $('#search-form').on('submit', function (ev) {
+        ev.preventDefault();
+        console.log('For submitted! But we are not doing anything - yet.');
+
+        let searchCriteria = $('#query').val();
+        console.log({ searchCriteria });
+
+        getRequest(searchCriteria);
+    });
+}
+
+function getRequest(searchCriteria) {
+
+        let url = 'https://www.googleapis.com/youtube/v3/search';
+        let data = {    part: 'snippet, id',
+                        q: searchCriteria,
+                        type: 'video',
+                        key: 'AIzaSyDt5iuCf8m9QH2C_6hwNQF76W_mFnsfers'
+        };
+
+        $.get(url, data, renderSearch);
+}
+    
+        
+function renderSearch (data) {
+        console.log('We got an answer:', data);
+        
+        var html = "";
+        var entries = data.items;
+
+        if (data.items.length === 0) {
+            $('.videos').append(`<p>Sorry, we can't find any results for that search criteria. Please search again.</p>`)
+            // show the user that we've got no results from YouTube
+        } else {
+            $.each(entries, function (index, data) {
+                let title = data.snippet.title;
+                let thumbnail = data.snippet.thumbnails.medium.url;
+                //variable holding data.part.property
+                let videoId = data.id.videoId;
+
+                html += '<a href="https://youtube.com/embed/' + videoId + '?rel=0">' + title + '</a>';
+                html += '<img src="' + thumbnail + '">';
+            }); 
+            
+            $('.videos').html(html);
+
+        }
+}
+
+
+$(initApp);
